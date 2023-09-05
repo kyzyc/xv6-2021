51c9ad5deeee172d17395f7f61ac783a362b58c6
zhuyuchen <yuchenzhu66@gmail.com>
2023-09-05 19:55:25 +0800

    finish print a page table

--- kernel/vm.c
+++ kernel/vm.c
@@ -432,3 +432,28 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+void vmprint_recur(pagetable_t pagetable, int deep)
+{
+  for(int i = 0; i < 512; i++) {
+    pte_t pte = pagetable[i];
+    if((pte & PTE_V) && (pte & (PTE_R|PTE_W|PTE_X)) == 0){
+      // this PTE points to a lower-level page table.
+      uint64 child = PTE2PA(pte);
+      if(deep == 1) {
+        printf("..");
+      }
+      printf(" ..%d: pte %p pa %p\n", i, pte, PTE2PA(pte));
+      vmprint_recur((pagetable_t)child, deep + 1);
+    } else if(pte & PTE_V){
+      printf(".. ..");
+      printf(" ..%d: pte %p pa %p\n", i, pte, PTE2PA(pte));
+    }
+  }
+}
+
+void vmprint(pagetable_t pagetable)
+{
+  printf("page table %p\n", pagetable);
+  vmprint_recur(pagetable, 0);
+}
